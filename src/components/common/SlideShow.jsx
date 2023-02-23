8f5afe60d16a0dbc764478c5ea7a5f933031b01c
Danel <danieldob200@gmail.com>
2023-02-23 06:33:46 -0600

    changed button margin to percent

--- src/components/common/SlideShow.jsx
+++ src/components/common/SlideShow.jsx
@@ -44,10 +44,10 @@ class SlideShow extends Component {
               <br /> increase efficiency and deliver freight solutions.
             </h2>
             <div className={classes.headerButtons}>
-              <Link to="/contact" className="btn">
+              <Link to='/contact' className="btn">
                 Ship with Sky City Trucking
               </Link>
-              <Link to="/careers" className="btn">
+              <Link to='/careers' className="btn">
                 Drive for Sky City Trucking
               </Link>
             </div>
