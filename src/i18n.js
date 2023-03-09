eae2d16778ef4c902abd31a624079c9f5ef0f435
Danel <danieldob200@gmail.com>
2023-03-09 16:14:50 -0600

    changed backend loadpath

--- src/i18n.js
+++ src/i18n.js
@@ -21,7 +21,7 @@ i18n.use(Backend)
             escapeValue: false // no need for react. it escapes by default
         },
         backend: {            
-            loadPath: '/locales/{{lng}}/translation.json'
+            loadPath: 'public/locales/{{lng}}/translation.json'
           },
     })
 
