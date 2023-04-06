98a5ce4f7a134536e94aff23e3e51c5c4401141d
Danel <danieldob200@gmail.com>
2023-04-06 13:21:34 -0500

    reverted

--- src/i18n.js
+++ src/i18n.js
@@ -24,8 +24,8 @@ import { initReactI18next } from "react-i18next";
 import Backend from "i18next-xhr-backend";
 import LanguageDetector from "i18next-browser-languagedetector";
 
-const fallbackLng = ["EN"];
-const availableLanguages = ["EN", "RU", "UK"];
+const fallbackLng = ["en"];
+const availableLanguages = ["en", "ru", "uk"];
 
 i18n
   .use(Backend)
