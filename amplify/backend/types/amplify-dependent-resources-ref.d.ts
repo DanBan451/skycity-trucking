ea890d49a10f0dff3c8ffab636a2512b8eaa149a
Danel <danieldob200@gmail.com>
2023-09-21 18:00:51 -0500

    many changes

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++
@@ -1,9 +0,0 @@
-export type AmplifyDependentResourcesAttributes = {
-  "api": {
-    "skycitytrucking": {
-      "GraphQLAPIEndpointOutput": "string",
-      "GraphQLAPIIdOutput": "string",
-      "GraphQLAPIKeyOutput": "string"
-    }
-  }
-}
